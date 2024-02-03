b72d26f354fed1edab07b381b5b5322f9e0472b2
ortega <kabweortega@gmail.com>
2024-02-03 11:41:02 +0200

    Build: Add remove redis dependency

--- src/main/java/com/ortega/scdn/repository/CdnServerRepository.java
+++ src/main/java/com/ortega/scdn/repository/CdnServerRepository.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.UUID;
 
-import static com.ortega.scdn.utils.Constants.SERVER_HASH_KEY;
-
 @Repository
 public class CdnServerRepository {
 
