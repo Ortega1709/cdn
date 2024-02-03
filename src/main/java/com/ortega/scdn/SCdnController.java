3b88c8de65c7ffc7601fb733174228d32625b20d
ortega <kabweortega@gmail.com>
2024-02-03 09:34:52 +0200

    Refactor: Change all application.properties by application.yml

--- src/main/java/com/ortega/scdn/SCdnController.java
+++ src/main/java/com/ortega/scdn/SCdnController.java
@@ -1,16 +1,12 @@
 package com.ortega.scdn;
 
-import com.ortega.scdn.models.CdnServer;
 import com.ortega.scdn.models.Location;
-import com.ortega.scdn.services.CdnServerService;
 import com.ortega.scdn.services.GeolocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("api/v1")
