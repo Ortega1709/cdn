2e0375778431caff448c0712db09a80cb1c2024b
ortega <kabweortega@gmail.com>
2024-02-05 09:35:29 +0200

    Build: Remove Eureka dependency

--- src/main/java/com/ortega/scdn/SCdnApplication.java
+++ src/main/java/com/ortega/scdn/SCdnApplication.java
@@ -3,6 +3,7 @@ package com.ortega.scdn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @EnableCaching
